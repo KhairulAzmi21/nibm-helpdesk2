4caea272d62c6c7a4301158a44eb867e0e221b64
khairul azmi <khairulfs21@gmail.com>
2017-04-04 15:12:32 +0800

    fix layout agent create

--- resources/views/vendor/ticketit/admin/agent/create.blade.php
+++ resources/views/vendor/ticketit/admin/agent/create.blade.php
@@ -28,7 +28,7 @@
                         <td>
                             <div class="checkbox">
                                 <label>
-                                    <input name="agents[]" type="checkbox" value="{{ $user->id }}" {!! $user->ticketit_agent ? "checked" : "" !!}> {{ $user->name }}
+                                    <input required name="agents[]" type="checkbox" value="{{ $user->id }}" {!! $user->ticketit_agent ? "checked" : "" !!}> {{ $user->name }}
                                 </label>
                             </div>
                         </td>
